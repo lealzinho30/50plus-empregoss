4edcbfa882b863e05c79f3e8e837ae82e7f95def
Seu Nome <seu-email@gmail.com>
2025-09-03 15:52:17 -0300

    Adicionar chat suporte e questionario perfil

---
+++ commit-simples.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Atualizar site"
+git push
---
+++ commit.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Fazendo commit das mudancas...
+cd /d "C:\Users\suzan\Downloads\LegalEssence\50plus-empregos-novo"
+git add .
+git commit -m "Adicionar chat suporte e questionario perfil"
+git push
+echo Commit realizado com sucesso!
+pause
